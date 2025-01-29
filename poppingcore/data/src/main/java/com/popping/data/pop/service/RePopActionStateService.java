0e9d9148832aa02334268ea0391df8388786d599
jang-199 <jw1010110@gmail.com>
2025-01-30 00:21:05 +0900

    repop

--- poppingcore/data/src/main/java/com/popping/data/pop/service/RePopActionStateService.java
+++ poppingcore/data/src/main/java/com/popping/data/pop/service/RePopActionStateService.java
@@ -1,10 +1,12 @@
 package com.popping.data.pop.service;
 
+import com.popping.data.pop.entity.PopActionState;
 import com.popping.data.pop.entity.RePopActionState;
 import com.popping.data.pop.repository.RePopActionStateRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
+import java.util.List;
 import java.util.Optional;
 
 @Service
@@ -30,5 +32,9 @@ public class RePopActionStateService {
 
     public Optional<RePopActionState> findEmotionState(Long memberPk, Long popPk) {
         return rePopActionStateRepository.findEmotionState(memberPk, popPk);
+    }
+
+    public List<RePopActionState> findActions(Long popPk) {
+        return rePopActionStateRepository.findActions(popPk);
     }
 }
