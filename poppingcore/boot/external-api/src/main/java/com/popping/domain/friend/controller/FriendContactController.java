d5b811656c881fb4532bcb43bfa434698d578d2b
LeeKing2017 <thrusum123@gmail.com>
2025-02-22 22:35:28 +0900

    feat: 연락처 기반으로 알수도 있는 친구 조회 API 구현

--- poppingcore/boot/external-api/src/main/java/com/popping/domain/friend/controller/FriendContactController.java
+++ poppingcore/boot/external-api/src/main/java/com/popping/domain/friend/controller/FriendContactController.java
@@ -1,23 +1,24 @@
 package com.popping.domain.friend.controller;
 
+import com.popping.domain.friend.dto.FindFriendContactDto;
 import com.popping.domain.friend.dto.SaveFriendContactDto;
+import com.popping.domain.friend.service.FindFriendContactService;
 import com.popping.domain.friend.service.SaveFriendContactService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.security.core.userdetails.UserDetails;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.net.URI;
+import java.util.List;
 
 @RestController
 @RequiredArgsConstructor
 @RequestMapping("/friends/contact")
 public class FriendContactController {
     private final SaveFriendContactService saveFriendContactService;
+    private final FindFriendContactService findFriendContactService;
 
     @PostMapping("")
     public ResponseEntity<Void> saveFriendContact(@RequestBody SaveFriendContactDto.Request request,
@@ -25,5 +26,10 @@ public class FriendContactController {
         saveFriendContactService.saveFriendContacts(Long.valueOf(userDetails.getUsername()), request);
 
         return ResponseEntity.created(URI.create("")).build();
+    }
+
+    @GetMapping("")
+    public ResponseEntity<List<FindFriendContactDto.Response>> findFriendsMayKnow(@AuthenticationPrincipal UserDetails userDetails) {
+        return ResponseEntity.ok(findFriendContactService.findFriendsMayKnow(Long.valueOf(userDetails.getUsername())));
     }
 }
--- poppingcore/data/src/main/java/com/popping/data/friendgroup/service/FriendContactService.java
+++ poppingcore/data/src/main/java/com/popping/data/friendgroup/service/FriendContactService.java
@@ -3,6 +3,7 @@ package com.popping.data.friendgroup.service;
 import com.popping.data.friendgroup.entity.FriendContact;
 import com.popping.data.friendgroup.repository.FriendContactRepository;
 import com.popping.data.member.entity.Member;
+import com.popping.data.member.service.MemberService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
@@ -12,6 +13,7 @@ import java.util.List;
 @RequiredArgsConstructor
 public class FriendContactService {
     private final FriendContactRepository friendContactRepository;
+    private final MemberService memberService;
 
     public void saveFriendContacts(Member owner, List<Member> friends) {
         List<FriendContact> friendContacts = friends.stream().map(
@@ -23,5 +25,10 @@ public class FriendContactService {
                 .toList();
 
         friendContactRepository.saveAll(friendContacts);
+    }
+
+    public List<Member> findFriends(Long requesterPk) {
+        List<Long> friendsPk = friendContactRepository.findFriends(requesterPk);
+        return memberService.findMembers(friendsPk);
     }
 }
