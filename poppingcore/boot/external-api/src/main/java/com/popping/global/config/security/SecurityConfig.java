d8fb02897cd0642ffada165d6499caa2da4ff4da
jang-199 <jw1010110@gmail.com>
2025-03-02 00:33:16 +0900

    asfd

--- poppingcore/boot/external-api/src/main/java/com/popping/global/config/security/SecurityConfig.java
+++ poppingcore/boot/external-api/src/main/java/com/popping/global/config/security/SecurityConfig.java
@@ -34,7 +34,7 @@ public class SecurityConfig {
                         .requestMatchers(HttpMethod.GET, "/tokens").permitAll()
                         .requestMatchers(HttpMethod.POST, "/sms/send").permitAll()
                         .requestMatchers(HttpMethod.POST, "/sms/verify").permitAll()
-                        .requestMatchers(HttpMethod.GET, "/invitations/info/**").permitAll()
+                        .requestMatchers(HttpMethod.GET, "/friends/invitations/info/**").permitAll()
                         .anyRequest().authenticated());
 
         httpSecurity
